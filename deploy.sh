3e7a1e94c1fe32e34814b33f93f62ba7ab62058d
Kostya Vasilyev <kmansoft@gmail.com>
2022-07-23 18:18:11 -0700

    Deployment fixes

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,10 @@
 ARCH=x86_64
 
 PACKDIR="./package"
-TEMPDIR="${PACKDIR}/temp-debian"
-EXENAME="longview-go.out"
+TEMPDIR="${PACKDIR}/out"
 
-VERSION=`cat ${PACKDIR}/VAR_VERSION | perl -ne 'chomp and print'`
-BUILD=`cat ${PACKDIR}/VAR_BUILD | perl -ne 'chomp and print'`
+VERSION=$(cat ${PACKDIR}/VAR_VERSION | perl -ne 'chomp and print')
+BUILD=$(cat ${PACKDIR}/VAR_BUILD | perl -ne 'chomp and print')
 
 if [[ "$ARCH" == "i686" ]]; then
     DEB_ARCH=i386
@@ -20,25 +19,6 @@ else
 fi
 
 OUT_DEB="longview-go_${VERSION}-${BUILD}_$DEB_ARCH.deb"
-
-SERVER="kman.mobi"
-if [[ $# -ge 1 ]]
-then
-    SERVER="$1"
-fi
-
-KEY = ""
-case "$SERVER" in
-    "kman.mobi")
-        KEY="2C6F9D8D-068A-D23C-1B87639441717CB1"
-        ;;
-    "clearview.rocks")
-        KEY="B6EEAB78-DAC1-12BB-55A06A8AD70521FF"
-        ;;
-    *)
-        echo "Unknown server ${SERVER}"
-        exit 1
-esac
 
 echo "*** Building Debian package ..."
 
@@ -60,22 +40,51 @@ fi
 echo "*** Version : ${VERSION}-${BUILD}"
 echo "*** Arch    : ${DEB_ARCH}"
 
-echo "*** Copying to ${SERVER} ..."
-
-if ! rsync -acv "${TEMPDIR}/${OUT_DEB}" "root@${SERVER}:"
+SERVER_LIST="$*"
+if [[ $# -eq 0 ]]
 then
-	echo "*** Error"
-	exit 1
+    SERVER_LIST="kman.mobi clearview.rocks node-eu-0001.email2-cloud.com node-na-0001.email2-cloud.com"
 fi
 
-echo "*** Installing ..."
+for SERVER in $SERVER_LIST
+do
 
-ssh -t "root@${SERVER}" "apt-get install --reinstall ./${OUT_DEB} && \
-    printf '$KEY\n' > /etc/linode/longview.key &&
-    printf 'username linode-longview\npassword longview\n' > /etc/linode/longview.d/MySQL.conf"
+  KEY=""
+  case "$SERVER" in
+      "kman.mobi")
+          KEY="2C6F9D8D-068A-D23C-1B87639441717CB1"
+          ;;
+      "clearview.rocks")
+          KEY="B6EEAB78-DAC1-12BB-55A06A8AD70521FF"
+          ;;
+      "node-eu-0001.email2-cloud.com")
+          KEY="BE99CFCC-EAEC-4B75-B7B2E2768C2C30D5"
+          ;;
+      "node-na-0001.email2-cloud.com")
+          KEY="21F055CB-B819-4455-A358308A6B5FC173"
+          ;;
+      *)
+          echo "Unknown server ${SERVER}"
+          exit 1
+  esac
 
-echo "*** Restarting ..."
+  echo "*** Copying to ${SERVER} ..."
 
-systemctl --host "root@${SERVER}" daemon-reload
-systemctl --host "root@${SERVER}" restart longview-go
+  if ! rsync -acv "${TEMPDIR}/${OUT_DEB}" "root@${SERVER}:"
+  then
+    echo "*** Error"
+    exit 1
+  fi
 
+  echo "*** Installing ..."
+
+  ssh -t "root@${SERVER}" "apt-get install --reinstall ./${OUT_DEB} && \
+      printf '$KEY\n' > /etc/linode/longview.key &&
+      printf 'username linode-longview\npassword longview\n' > /etc/linode/longview.d/MySQL.conf"
+
+  echo "*** Restarting ..."
+
+  systemctl --host "root@${SERVER}" daemon-reload
+  systemctl --host "root@${SERVER}" restart longview-go
+
+done
